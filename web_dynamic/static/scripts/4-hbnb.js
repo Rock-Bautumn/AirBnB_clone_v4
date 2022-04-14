214215c18bb15426578c38b9f7a37153fe4b3089
Rock-Bautumn <3623@holbertonschool.com>
2022-04-13 20:51:17 -0500

    Adding changes for task 5

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -3,27 +3,23 @@ function pluralize (string, num) {
   else { return `${string}s`}
 }
 
-$(document).ready(function () {
-  const amenities = {};
-  $.get('http://0.0.0.0:5001/api/v1/status/', (returnData) => {
-    // console.log(returnData);
-    if (returnData.status === 'OK') {
-      $('#api_status').attr('class', 'available')
-    }
-  });
-  console.log("before");
+function doQuery (amenitiesList) {
+  // console.log(JSON.stringify(amenitiesList))
+  amenitiesList = JSON.stringify({'amenities': amenitiesList})
+  // console.log(amenitiesList)
   $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/places_search',
     type: 'POST',
-    data: '{}',
+    data: amenitiesList,
     dataType: 'json',
     contentType: 'application/json',
     success: function (data) {
-        console.log(data)
+        // console.log(data)
+        $('section.places').empty();
         let i = ""
         for (let i in data) {
             let place = data[i];
-            console.log(`place is ${place.name}`)
+            // console.log(`place is ${place.name}`)
             $('section.places').append(
               `
               <article>
@@ -44,7 +40,54 @@ $(document).ready(function () {
             </article>
               `
             )
-            console.log (place.name)
+            // console.log (place.name)
+          }
+    }
+  })
+}
+
+$(document).ready(function () {
+  const amenities = {};
+  $.get('http://0.0.0.0:5001/api/v1/status/', (returnData) => {
+    // console.log(returnData);
+    if (returnData.status === 'OK') {
+      $('#api_status').attr('class', 'available')
+    }
+  });
+  // console.log("before");
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    type: 'POST',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+        // console.log(data)
+        let i = ""
+        for (let i in data) {
+            let place = data[i];
+            // console.log(`place is ${place.name}`)
+            $('section.places').append(
+              `
+              <article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">$${place.price_by_night}</div>
+              </div>
+              <div class="information">
+                <div class="max_guest">${place.max_guest} ${pluralize('Guest', place.max_guest)}</div>
+                      <div class="number_rooms">${place.number_rooms} ${pluralize('Bedroom', place.number_rooms)}</div>
+                      <div class="number_bathrooms">${place.number_bathrooms} ${pluralize('Bathroom', place.number_bathrooms)}</div>
+              </div>
+              <div class="user">
+                    </div>
+                    <div class="description">
+                ${place.description}
+                    </div>
+            </article>
+              `
+            )
+            // console.log (place.name)
           }
     }
   })
@@ -58,7 +101,7 @@ $(document).ready(function () {
   // $.post('http://0.0.0.0:5001/api/v1/places_search/', sendingData, function(recvdData) {
   //   console.log(recvdData);
   // });
-  console.log("okay");
+  // console.log("okay");
 
   $('input:checkbox').click(function () {
     $(this).each(function () {
@@ -68,10 +111,28 @@ $(document).ready(function () {
         delete amenities[$(this).data('id')];
       }
     });
-    if (sendingDataect.values(amenities).length > 0) {
-      $('.amenities h4').text(sendingDataect.values(amenities).join(', '));
+    if (Object.values(amenities).length > 0) {
+      $('.amenities h4').text(Object.values(amenities).join(', '));
     } else {
       $('.amenities h4').html('&nbsp');
     }
+  });
+
+  $('.filters > button').click(function () {
+    // console.log('clicked it!!')
+    let outList = [];
+    $('.amenities input:checkbox').each(function () {
+      // console.log('here?')
+      if (this.checked) {
+        // console.log(`data id: ${$(this).attr('data-id')}`)
+        outList.push($(this).attr('data-id'));
+      }
+      else {
+        // console.log(`missed: ${this.checked}`)
+      }
+      
+    });
+    // console.log(outList);
+    doQuery(outList)
   });
 });
