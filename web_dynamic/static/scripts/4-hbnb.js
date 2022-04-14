4e3f92ba8fdf946d34bdce24464afffa4797a061
Rock-Bautumn <3623@holbertonschool.com>
2022-04-13 20:53:51 -0500

    cleaning up duplicate code in 4-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -54,54 +54,7 @@ $(document).ready(function () {
       $('#api_status').attr('class', 'available')
     }
   });
-  // console.log("before");
-  $.ajax({
-    url: 'http://0.0.0.0:5001/api/v1/places_search',
-    type: 'POST',
-    data: '{}',
-    dataType: 'json',
-    contentType: 'application/json',
-    success: function (data) {
-        // console.log(data)
-        let i = ""
-        for (let i in data) {
-            let place = data[i];
-            // console.log(`place is ${place.name}`)
-            $('section.places').append(
-              `
-              <article>
-              <div class="title_box">
-                <h2>${place.name}</h2>
-                <div class="price_by_night">$${place.price_by_night}</div>
-              </div>
-              <div class="information">
-                <div class="max_guest">${place.max_guest} ${pluralize('Guest', place.max_guest)}</div>
-                      <div class="number_rooms">${place.number_rooms} ${pluralize('Bedroom', place.number_rooms)}</div>
-                      <div class="number_bathrooms">${place.number_bathrooms} ${pluralize('Bathroom', place.number_bathrooms)}</div>
-              </div>
-              <div class="user">
-                    </div>
-                    <div class="description">
-                ${place.description}
-                    </div>
-            </article>
-              `
-            )
-            // console.log (place.name)
-          }
-    }
-  })
-  // let sendingData = {};
-  // sendingData.name = "Raj";
-  // sendingData.age  = 32;
-  // sendingData.married = false;
-  // sendingData = JSON.stringify(sendingData);
-
-  // post(URL,data,function(data,status,xhr),dataType)
-  // $.post('http://0.0.0.0:5001/api/v1/places_search/', sendingData, function(recvdData) {
-  //   console.log(recvdData);
-  // });
-  // console.log("okay");
+  doQuery([]);
 
   $('input:checkbox').click(function () {
     $(this).each(function () {
